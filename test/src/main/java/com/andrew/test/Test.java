6212fc6bcbde86d08ae4cb61eb582815fd33a2d9
Andrew <2430209342@qq.com>
2021-03-27 09:23:34 +0800

    service

--- test/src/main/java/com/andrew/test/Test.java
+++ test/src/main/java/com/andrew/test/Test.java
@@ -1,8 +1,19 @@
 package com.andrew.test;
 
+import cn.hutool.core.map.MapBuilder;
+import cn.hutool.core.map.MapUtil;
+import org.apache.commons.beanutils.BeanMap;
+
+import java.util.HashMap;
+
 public class Test {
 
     public static void main(String[] args) {
+
+
+        BeanMap beanMap = new BeanMap(MapUtil.builder().put("name", "Andrew").build());
+        Object name = beanMap.get("name");
+        System.out.println(name);
 
     }
 
